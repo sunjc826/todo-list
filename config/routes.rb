670efc112058714b7db736e76e4e707401d022dc
sunjc826 <sunjc826@gmail.com>
2020-12-17 13:01:59 +0800

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,20 @@ Rails.application.routes.draw do
   get "/welcome", to: "welcome#index"
   get "/welcome/index", to: "welcome#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :comments, only: [:create, :update, :destroy]
+      resources :filter_criteria
+      resources :filters
+      resources :label_tasks
+      resources :labels
+      resources :projects
+      resources :subtasks, only: [:create, :update, :destroy]
+      resources :tag_tasks
+      resources :tags
+      resources :tasks
+      resources :users
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

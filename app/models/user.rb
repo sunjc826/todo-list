e589bbc66a1523f23aef62c7b95b6158ccfd6859
sunjc826 <sunjc826@gmail.com>
2020-12-21 16:43:05 +0800

    Begin work on authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
   has_many :labels
   has_many :filters
   has_many :projects
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_153210) do
+ActiveRecord::Schema.define(version: 2020_12_20_151929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_153210) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "tasks"

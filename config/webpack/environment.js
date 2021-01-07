1f73c7fe69b585f4e86bcc53ce7d3a10239175b2
sunjc826 <sunjc826@gmail.com>
2021-01-07 21:05:47 +0800

    Typescript webpacker working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require("@rails/webpacker");
 
-const tsLoader = {
-  test: /.(ts|tsx)$/,
-  use: "ts-loader",
-};
-
-environment.loaders.append("typescript", tsLoader);
-
 module.exports = environment;
--- decs.d.ts
+++ decs.d.ts
@@ -1,2 +1,3 @@
 // https://medium.com/@steveruiz/using-a-javascript-library-without-type-declarations-in-a-typescript-project-3643490015f3
 declare module "json-api-normalizer";
+// declare module "*";
